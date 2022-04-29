cc5ec2c2705020fe98e2701d83327bfcecc2a0cd
yasmikash <yashtricksz@gmail.com>
2022-04-29 15:13:20 +0530

    add ch

--- app.py
+++ app.py
@@ -19,3 +19,6 @@ def home():
     provided_breathing_signal = pd.read_csv("models/breathing/data/fast.csv")["read"].values
     peaks, probs = breathing_detector.find_peaks(provided_breathing_signal[:500])
     return jsonify(peaks.tolist())
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
